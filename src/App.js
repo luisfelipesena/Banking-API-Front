e793ba79c7bd27cdaa697881599b7a22fb5eaf73
Luis Felipe <luisfelipesena@gmail.com>
2020-11-21 00:45:15 -0300

    feat: nova organização de arquivos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div>oi</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
