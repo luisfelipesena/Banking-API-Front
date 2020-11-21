3d332095e7bff0ccf376cb64bbff91bfe2963961
Luis Felipe <luisfelipesena@gmail.com>
2020-11-21 17:17:29 -0300

    feat: esqueci-senha useSenha()

--- src/components/esqueci-senha/esqueci-senha.js
+++ src/components/esqueci-senha/esqueci-senha.js
@@ -2,7 +2,19 @@ import { UseFetch } from "../../utils/fetch";
 
 export function useSenha() {
   async function esqueciSenha(email) {
-    return true;
+    const request = await UseFetch(
+      `https://cubos-banking-api.herokuapp.com/esqueci-senha`, //Implementar em Back
+      "POST",
+      {
+        email,
+      }
+    );
+    const response = await request.json();
+    if (response.dados && response.dados.id) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   return { esqueciSenha };
