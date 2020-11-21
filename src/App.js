a7bb96ec8f55addeb1219babf5a23b45542ae28b
Luis Felipe <luisfelipesena@gmail.com>
2020-11-21 07:08:31 -0300

    feat: routes /; /cadastro and /esqueci-senha added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Login } from "./pages/login-cadastro/login/index";
+import { Cadastro } from "./pages/login-cadastro/cadastro/index";
+import { EsqueciSenha } from "./pages/login-cadastro/senha/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={<div> Hello</div>} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/cadastro" component={Cadastro} />
+        <Route exact path="/esqueci-senha" component={EsqueciSenha} />
       </Switch>
     </BrowserRouter>
   );
