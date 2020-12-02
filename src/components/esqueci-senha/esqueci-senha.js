7430b8913f8981fd4409fdad133069878151806e
Luis Felipe <luisfelipesena@gmail.com>
2020-12-01 22:02:37 -0300

    fix: resetPasswors routes

--- src/components/esqueci-senha/esqueci-senha.js
+++ src/components/esqueci-senha/esqueci-senha.js
@@ -3,7 +3,7 @@ import { UseFetch } from "../../utils/fetch";
 export function useSenha() {
   async function esqueciSenha(email) {
     const request = await UseFetch(
-      `https://cubos-banking-api.herokuapp.com/usuarios/resetPassword`,
+      `https://cubos-banking-api.herokuapp.com/usuarios/resetPasswordEmail`,
       "POST",
       {
         email,
--- src/components/trocar-senha/trocar-senha.js
+++ src/components/trocar-senha/trocar-senha.js
@@ -2,7 +2,7 @@ import { UseFetch } from "../../utils/fetch";
 
 export const TrocarSenha = async (senha, id) => {
   const response = await UseFetch(
-    "https://cubos-banking-api.herokuapp.com/usuarios",
+    "https://cubos-banking-api.herokuapp.com/resetPassword",
     "PUT",
     {
       senha,
